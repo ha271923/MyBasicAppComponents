e1536b68474e9916b4225102e77798aea5d1df59
hawk_wei <hawk_wei@htc.com>
2016-08-09 19:18:31 +0800

    Show imageView on ListView

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/MyListRowDataItem.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/MyListRowDataItem.java
@@ -6,13 +6,15 @@ package sample.hawk.com.mybasicappcomponents;
 
 public class MyListRowDataItem {
     private int type;
+    private int photoId;
     private String name;
     private String time;
 
-    public MyListRowDataItem(int type, String name, String time) {
+    public MyListRowDataItem(int type, int photoId, String name, String time) {
         this.type = type;
         this.name = name;
         this.time = time;
+        this.photoId = photoId;
     }
     // 想要傳入ListView的資料有:頻道名稱 電影名稱 電影時間
     // 希望可以很明顯看到每個list item長的並不是很相同,所以額外加了一個type的參數
@@ -22,6 +24,8 @@ public class MyListRowDataItem {
     //   type 2 = 這個item是電影 + 顏色是黃色
     public int    getType(){ return type; }
     public void   setType(int type){ this.type = type; }
+    public int getPhotoId(){return photoId;}
+    public void   setPhotoId(int photoId){ this.photoId = photoId; }
     public String getName(){ return name; }
     public void   setName(String name){ this.name = name; }
     public String getTime(){ return time; }
--- app/src/main/java/sample/hawk/com/mybasicappcomponents/MyListViewActivity.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/MyListViewActivity.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -28,37 +29,36 @@ public class MyListViewActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Create DB
-        rowItems.add(new MyListRowDataItem(0,"HBO電影台","")); // Channel 1
-        rowItems.add(new MyListRowDataItem(1,"綠光戰警","7:00"));
-        rowItems.add(new MyListRowDataItem(2,"鋼鐵人","9:00"));
-        rowItems.add(new MyListRowDataItem(1,"蝙蝠俠:開戰時刻","11:00"));
-
-        rowItems.add(new MyListRowDataItem(0,"Discovery","")); // Channel 2
-        rowItems.add(new MyListRowDataItem(1,"動物生死鬥","7:30"));
-        rowItems.add(new MyListRowDataItem(2,"超級獵人","9:00"));
-        rowItems.add(new MyListRowDataItem(1,"阿貓阿狗","12:00"));
-        rowItems.add(new MyListRowDataItem(1,"Lion King","13:30"));
-        rowItems.add(new MyListRowDataItem(0,"衛視電影台","")); // Channel 3
-        rowItems.add(new MyListRowDataItem(1,"海角七號","7:00"));
-        rowItems.add(new MyListRowDataItem(2,"陣頭","9:00"));
-        rowItems.add(new MyListRowDataItem(1,"星空","11:00"));
-        rowItems.add(new MyListRowDataItem(1,"我的少女時代","14:00"));
-        rowItems.add(new MyListRowDataItem(1,"英雄","18:00"));
-        rowItems.add(new MyListRowDataItem(1,"十面埋伏","21:00"));
-        rowItems.add(new MyListRowDataItem(0,"東森新聞台","")); // Channel 4
-        rowItems.add(new MyListRowDataItem(1,"午間新聞","11:30"));
-        rowItems.add(new MyListRowDataItem(2,"晚間新聞","9:00"));
-        rowItems.add(new MyListRowDataItem(1,"晨間新聞","6:00"));
-        rowItems.add(new MyListRowDataItem(0,"ESPN","")); // Channel 5
-        rowItems.add(new MyListRowDataItem(1,"NBA stuff","8:30"));
-        rowItems.add(new MyListRowDataItem(2,"NBA Final Game7","9:00"));
-        rowItems.add(new MyListRowDataItem(1,"F1 Race","11:00"));
-        rowItems.add(new MyListRowDataItem(1,"Golf World","13:30"));
+        rowItems.add(new MyListRowDataItem(0, 0 , "HBO電影台","")); // Channel 1
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_1, "綠光戰警","7:00"));
+        rowItems.add(new MyListRowDataItem(2,R.drawable.gallery_photo_2, "鋼鐵人","9:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_3, "蝙蝠俠:開戰時刻","11:00"));
+        rowItems.add(new MyListRowDataItem(0,0,"Discovery","")); // Channel 2
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_4, "動物生死鬥","7:30"));
+        rowItems.add(new MyListRowDataItem(2,R.drawable.gallery_photo_5, "超級獵人","9:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_6, "阿貓阿狗","12:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_7, "Lion King","13:30"));
+        rowItems.add(new MyListRowDataItem(0,0,"衛視電影台","")); // Channel 3
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_8, "海角七號","7:00"));
+        rowItems.add(new MyListRowDataItem(2,R.drawable.gallery_photo_1, "陣頭","9:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_2, "星空","11:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_3, "我的少女時代","14:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_4, "英雄","18:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_5, "十面埋伏","21:00"));
+        rowItems.add(new MyListRowDataItem(0,0, "東森新聞台","")); // Channel 4
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_7, "午間新聞","11:30"));
+        rowItems.add(new MyListRowDataItem(2,R.drawable.gallery_photo_6, "晚間新聞","9:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_5, "晨間新聞","6:00"));
+        rowItems.add(new MyListRowDataItem(0,0,"ESPN","")); // Channel 5
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_3, "NBA stuff","8:30"));
+        rowItems.add(new MyListRowDataItem(2,R.drawable.gallery_photo_4, "NBA Final Game7","9:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_7, "F1 Race","11:00"));
+        rowItems.add(new MyListRowDataItem(1,R.drawable.gallery_photo_2, "Golf World","13:30"));
 
         // Get DB Adapter
         adapter = new MyListDataAdapter(this, rowItems);
         // Link DB with View
-        setContentView(R.layout.mylist_activity);
+        setContentView(R.layout.mylist_activity); // your listview page layout
         listV=(ListView)findViewById(R.id.myactivitylist);
         listV.setAdapter(adapter);
     }
@@ -85,7 +85,9 @@ public class MyListViewActivity extends ListActivity {
         private class ViewHolder {
             TextView txtTitle;
             TextView txtTime;
-            public ViewHolder(TextView txtTitle, TextView txtTime){
+            ImageView picPhoto;
+            public ViewHolder(ImageView picPhoto, TextView txtTitle, TextView txtTime){
+                this.picPhoto = picPhoto;
                 this.txtTitle = txtTitle;
                 this.txtTime = txtTime;
             }
@@ -96,8 +98,9 @@ public class MyListViewActivity extends ListActivity {
             ViewHolder holder = null;
 
             if(convertView==null){
-                convertView = myInflater.inflate(R.layout.mylist_rowitem, null);
+                convertView = myInflater.inflate(R.layout.mylist_rowitem, null); // one listview item's view layout
                 holder = new ViewHolder(
+                        (ImageView) convertView.findViewById(R.id.photo),
                         (TextView) convertView.findViewById(R.id.title),
                         (TextView) convertView.findViewById(R.id.time)
                 );
@@ -112,15 +115,21 @@ public class MyListViewActivity extends ListActivity {
             MyListRowDataItem movie = (MyListRowDataItem)getItem(position);
             // type_num={      [0]:channel, [1]:movie,   [2]:time }
             int type_num = movie.getType();
+            int image_vis[] =  {View.GONE,  View.VISIBLE,View.VISIBLE};
             int color_title[] ={Color.WHITE,Color.WHITE, Color.YELLOW};
             int color_time[] = {Color.WHITE,Color.WHITE, Color.YELLOW};
             int color_back[] = {Color.BLUE, Color.BLACK, Color.BLACK};
             int time_vis[] =   {View.GONE,  View.VISIBLE,View.VISIBLE};
 
+            int picPhotoId = movie.getPhotoId();
+            holder.picPhoto.setImageResource(picPhotoId);
+            holder.picPhoto.setVisibility(image_vis[type_num]);
+
             String strTitle = movie.getName();
             holder.txtTitle.setText(strTitle);
             holder.txtTitle.setTextColor(color_title[type_num]);
             holder.txtTitle.setBackgroundColor(color_back[type_num]);
+
             String strTime = movie.getTime();
             holder.txtTime.setText(strTime);
             holder.txtTime.setTextColor(color_time[type_num]);
