de1f8ba5f0db3e4a5d1fad840a6792e763a0575f
hawk_wei <hawk_wei@htc.com>
2016-10-14 15:00:33 +0800

    OK: verify synchronized(this)  keyword, implements Runnable (NOT extends Thread) set ThreadName --> OS --> get ThreadName

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/oo/MySyncTestThread2.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/oo/MySyncTestThread2.java
@@ -16,13 +16,13 @@ public class MySyncTestThread2 implements Runnable{
     MySyncTestThread2(String name, boolean sync) {
         // mThreadName = Thread.currentThread().getName();
         mSync = sync;
-        SMLog.i(TAG,mThreadName+"constructor");
+        // SMLog.i(TAG,mThreadName+"constructor");
     }
 
     public void run() {
         int var=0; // stack_var
-        mThreadName = Thread.currentThread().getName();
-        SMLog.i(TAG, "mSync="+mSync+"  run +++    name=" + mThreadName);
+        String ThreadName = Thread.currentThread().getName();
+        SMLog.i(TAG, "mSync="+mSync+"  run +++    name=" + ThreadName);
         if(mSync) {
             synchronized(this) { // synchronized +++
                 for (int i = 0; i < 10; i++) {
@@ -30,9 +30,9 @@ public class MySyncTestThread2 implements Runnable{
                         static_var++;var++;CommonResources.static_var++;CommonResources.StaticInnerClass.static_var++;
                         // CommonResources.var++; CommonResources.InnerClass.var++; CommonResources.StaticInnerClass.var++; // ERROR: Non-static field var 'var' cannot be reference from a static context.
                         Thread.sleep(100); // Hawk: simulate context-switch
-                        //SMLog.i(TAG, "run        name="+mThreadName+"  var="+ var+"  static_var="+ static_var);
-                        SMLog.i(TAG, "run        name="+mThreadName+"  var="+ var+"  static_var="+ static_var+"  CommonResources.static_var="+CommonResources.static_var);
-                        //SMLog.i(TAG, "run        name="+mThreadName+"  CommonResources.static_var="+CommonResources.static_var+"  CommonResources.StaticInnerClass.static_var="+CommonResources.StaticInnerClass.static_var);
+                        //SMLog.i(TAG, "run        name="+ThreadName+"  var="+ var+"  static_var="+ static_var);
+                        SMLog.i(TAG, "run        name="+ThreadName+"  var="+ var+"  static_var="+ static_var+"  CommonResources.static_var="+CommonResources.static_var);
+                        //SMLog.i(TAG, "run        name="+ThreadName+"  CommonResources.static_var="+CommonResources.static_var+"  CommonResources.StaticInnerClass.static_var="+CommonResources.StaticInnerClass.static_var);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -45,14 +45,14 @@ public class MySyncTestThread2 implements Runnable{
                     static_var++;var++;CommonResources.static_var++;
                     // CommonResources.var++; // ERROR: Non-static field var 'var' cannot be reference from a static context.
                     Thread.sleep(100); // Hawk: simulate context-switch
-                    //SMLog.i(TAG, "run        name="+mThreadName+"  var="+ var+"  static_var="+ static_var);
-                    SMLog.i(TAG, "run        name="+mThreadName+"  var="+ var+"  static_var="+ static_var+"  CommonResources.static_var="+CommonResources.static_var);
-                    //SMLog.i(TAG, "run        name="+mThreadName+"  CommonResources.static_var="+CommonResources.static_var+"  CommonResources.StaticInnerClass.static_var="+CommonResources.StaticInnerClass.static_var);
+                    //SMLog.i(TAG, "run        name="+ThreadName+"  var="+ var+"  static_var="+ static_var);
+                    SMLog.i(TAG, "run        name="+ThreadName+"  var="+ var+"  static_var="+ static_var+"  CommonResources.static_var="+CommonResources.static_var);
+                    //SMLog.i(TAG, "run        name="+ThreadName+"  CommonResources.static_var="+CommonResources.static_var+"  CommonResources.StaticInnerClass.static_var="+CommonResources.StaticInnerClass.static_var);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
-        SMLog.i(TAG, "          run    --- name=" + mThreadName);
+        SMLog.i(TAG, "          run    --- name=" + ThreadName);
     }
 }
