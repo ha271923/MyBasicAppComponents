815b308af549e6e6097e8a8d5e7ba6e33f01b988
hawk_wei <hawk_wei@htc.com>
2016-12-02 11:47:59 +0800

    Fix

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyLayoutActivity.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyLayoutActivity.java
@@ -1,12 +1,11 @@
 package sample.hawk.com.mybasicappcomponents.view;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.TableLayout;
 
 import sample.hawk.com.mybasicappcomponents.R;
 
@@ -15,21 +14,34 @@ import sample.hawk.com.mybasicappcomponents.R;
  */
 
 public class MyLayoutActivity extends Activity {
+    RelativeLayout m_rootLayout;
+    LinearLayout m_layer2Layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mylayout);
-
-        LinearLayout rootLayout = (LinearLayout) findViewById(R.id.my_layout);
-        getLayoutInflater().inflate(R.layout.myscrollview, rootLayout);
-        LinearLayout layer2Layout = (LinearLayout) findViewById(R.id.my_scrolllist);
-            getLayoutInflater().inflate(R.layout.mylinearlayout, layer2Layout);
-            getLayoutInflater().inflate(R.layout.myrelativelayout, layer2Layout);
-            getLayoutInflater().inflate(R.layout.myframelayout, layer2Layout);
-            getLayoutInflater().inflate(R.layout.mytablelayout, layer2Layout);
-            getLayoutInflater().inflate(R.layout.mygridlayout, layer2Layout);
-            getLayoutInflater().inflate(R.layout.myabsolutelayout, layer2Layout);
-
+        layout();
     }
+
+    private void layout(){
+        m_rootLayout = (RelativeLayout) findViewById(R.id.my_layout);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            getLayoutInflater().inflate(R.layout.myscrollview, m_rootLayout);
+                m_layer2Layout = (LinearLayout) findViewById(R.id.my_scrolllist);
+        }
+        else{
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            getLayoutInflater().inflate(R.layout.myscrollview_horizontal, m_rootLayout);
+            m_layer2Layout = (LinearLayout) findViewById(R.id.my_scrolllist_horizontal);
+        }
+            getLayoutInflater().inflate(R.layout.mylinearlayout, m_layer2Layout);
+            getLayoutInflater().inflate(R.layout.myrelativelayout, m_layer2Layout);
+            getLayoutInflater().inflate(R.layout.myframelayout, m_layer2Layout);
+            getLayoutInflater().inflate(R.layout.mytablelayout, m_layer2Layout);
+            getLayoutInflater().inflate(R.layout.mygridlayout, m_layer2Layout);
+            getLayoutInflater().inflate(R.layout.myabsolutelayout, m_layer2Layout);
+    }
+
 }
