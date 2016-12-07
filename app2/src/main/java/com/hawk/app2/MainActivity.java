34b853bdcf84413fc0e35c7b644c1e19c056f80c
hawk_wei <hawk_wei@htc.com>
2016-12-07 17:20:27 +0800

    App Module: Custom View for ValueSelector, I can verify this component in App2. https://github.com/IntertechInc/android-custom-view-tutorial

--- app2/src/main/java/com/hawk/app2/MainActivity.java
+++ app2/src/main/java/com/hawk/app2/MainActivity.java
@@ -2,6 +2,8 @@ package com.hawk.app2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity);
+
+        final ValueSelector valueSelector = (ValueSelector) findViewById(R.id.valueSelector);
+        valueSelector.setMinValue(0);
+        valueSelector.setMaxValue(100);
+
+        final ValueBar valueBar = (ValueBar) findViewById(R.id.valueBar);
+        valueBar.setMaxValue(100);
+        valueBar.setAnimated(true);
+        valueBar.setAnimationDuration(4000l);
+
+        Button updateButton = (Button) findViewById(R.id.updateButton);
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int value = valueSelector.getValue();
+                valueBar.setValue(value);
+
+                //code to use Object Animation instead of the built-in ValueBar animation
+                //if you use this, be sure the call valueBar.setAnimated(false);
+                /*
+                ObjectAnimator anim = ObjectAnimator.ofInt(valueBar, "value", valueBar.getValue(), value);
+                anim.setDuration(1000);
+                anim.start();
+                */
+            }
+        });
     }
 }
