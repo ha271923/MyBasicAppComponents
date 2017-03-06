b539fd3c4ae4485a14f567b00b382732cf85c560
hawk_wei <hawk_wei@htc.com>
2017-03-06 18:12:54 +0800

    MySQL: a simple SQL example for ADD, EDIT, DELETE operations.

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/provider/DB/MySQL_Activity.java
@@ -0,0 +1,128 @@
+package sample.hawk.com.mybasicappcomponents.provider.DB;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import sample.hawk.com.mybasicappcomponents.R;
+
+public class MySQL_Activity extends Activity {
+    // Buuton res in R.menu.mysql_menu
+    private Button add , edit, del, clear, end;
+    private EditText edname , edprice;
+    private MySQL_DB db = null;
+    private Cursor mCursor;
+    private ListView listview;
+    private long select_item = 1; //item 預設值 = 1
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mysql_activity);
+        add = (Button) findViewById(R.id.add); add.setOnClickListener(Buttonslistener);
+        edit = (Button) findViewById(R.id.edit); edit.setOnClickListener(Buttonslistener);
+        del = (Button) findViewById(R.id.del); del.setOnClickListener(Buttonslistener);
+        clear = (Button) findViewById(R.id.clear); clear.setOnClickListener(Buttonslistener);
+        end =(Button) findViewById(R.id.end); end.setOnClickListener(Buttonslistener);
+        edname = (EditText) findViewById(R.id.edname);
+        edprice = (EditText) findViewById(R.id.edprice);
+        listview = (ListView) findViewById(R.id.listview); listview.setOnItemClickListener(Listviewlistener);
+        db = new MySQL_DB(this);
+        db.open();
+        mCursor = db.getALL();
+        UpdateAdapter(mCursor);
+    }
+
+    //將傳回的Cursor塞進listview
+    private void UpdateAdapter(Cursor c) {
+        if (c != null && c.getCount() > 0) {
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, c,
+                    new String[] {          "name",      "price"       },
+                    new int[] { android.R.id.text1, android.R.id.text2 });
+            listview.setAdapter(adapter);
+        }
+    }
+
+    //ListView listener
+    private OnItemClickListener Listviewlistener = new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            select_item = arg3;
+            find_data(select_item);
+            //將Cursor移動到id的位置 不過似乎沒加上這行對app也沒差
+            //mCursor.moveToPosition(arg2);
+        }
+
+        //依照id查詢資料
+        private void find_data(long search_id) {
+            Cursor c = db.getsearchid(search_id);
+            // show it!
+            edname.setText(c.getString(1));
+            edprice.setText(c.getString(2));
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
+
+    //Buttons listener
+    private OnClickListener Buttonslistener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.add:
+                    if (db.add(edname.getText().toString(), edprice.getText().toString()) > 0) {
+                        mCursor = db.getALL();
+                        UpdateAdapter(mCursor);
+                        clearedit();
+                    }
+                    break;
+                case R.id.edit:
+                    if (db.edit(select_item, edname.getText().toString(), edprice.getText().toString())) {
+                        mCursor = db.getALL();
+                        UpdateAdapter(mCursor);
+                        clearedit();
+                    }
+                    break;
+                case R.id.del:
+                    if (db.delete(select_item)) {
+                        mCursor = db.getALL();
+                        UpdateAdapter(mCursor);
+                        clearedit();
+                    }
+                    break;
+                case R.id.clear:
+                    clearedit();
+                    break;
+                case R.id.end:
+                    finish();
+                    break;
+            }
+        }
+
+        private void clearedit() {
+            edname.setText("");
+            edprice.setText("");
+        }
+
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mysql_menu, menu);
+        return true;
+    }
+}
---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/provider/DB/MySQL_DB.java
@@ -0,0 +1,88 @@
+package sample.hawk.com.mybasicappcomponents.provider.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import sample.hawk.com.mybasicappcomponents.utils.SMLog;
+
+// Excel-like table
+//     |    A   |     B   |   C    |
+//----------------------------------
+//  1  |   id1  |   name1 | price1 |
+//----------------------------------
+//  2  |   id2  |   name2 | price2 |
+//----------------------------------
+//  3  |   id3  |   name3 | price3 |
+//----------------------------------
+//  4  |   id4  |   name4 | price4 |
+//----------------------------------
+public class MySQL_DB {
+    private Context mContext = null;
+    public SQLiteDatabase db = null;
+    private final static String DATABASE_NAME = "db1.db";
+    private final static String TABLE_NAME = "table01";
+    private final static String _ID = "_id";
+    private final static String NAME = "name";
+    private final static String PRICE = "price";
+    private final static String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME +
+            " (" + _ID   + " INTEGER PRIMARY KEY,"
+                 + NAME  + " TEXT,"
+                 + PRICE + " TEXT)";
+
+    public MySQL_DB(Context ctx) {
+        this.mContext = ctx;
+    }
+    //Create Or Open Table
+    public void open() throws SQLException {
+        db = mContext.openOrCreateDatabase(DATABASE_NAME, 0, null);
+        try {
+            db.execSQL(CREATE_TABLE);
+        } catch (Exception e) {
+        }
+    }
+    //關閉DB
+    public void close() throws SQLException {
+        db.close();
+    }
+    //查詢全部
+    public Cursor getALL() {
+        Cursor c = db.query(TABLE_NAME, new String[] { _ID, NAME, PRICE }, null, null, null, null, null);
+        while (c.moveToNext()){
+            SMLog.i("[GETALL]  "+ c.getString(0) +"  |  "+c.getString(1)+"  |  "+c.getString(2));
+        }
+        return c;
+    }
+    //單筆查詢
+    public Cursor getsearchid(long id) throws SQLException {
+        Cursor c = db.query(TABLE_NAME, new String[] { _ID, NAME, PRICE }, _ID + "=" + id, null, null, null, null, null);
+        if (c != null && c.getCount() > 0) {
+            c.moveToNext();
+            SMLog.i("[getsearchid]  "+ c.getString(0) +"  |  "+c.getString(1)+"  |  "+c.getString(2));
+        }
+        return c;
+    }
+    //回傳long Type用來判斷新增是否成功和增加的筆數
+    public long add(String name, String price) {
+        ContentValues args = new ContentValues();
+        args.put(NAME, name);
+        args.put(PRICE, price);
+        SMLog.i("[ADD] name= "+ name + " price= "+ price);
+        return db.insert(TABLE_NAME, null, args);
+    }
+    public boolean delete(long id) {
+        SMLog.i("[DEL] ID= "+ id);
+        return db.delete(TABLE_NAME, _ID + "=" + id, null) > 0;
+    }
+    //boolean回傳1代表更新成功
+    public boolean edit(long id, String name, String price) {
+        ContentValues args = new ContentValues();
+        args.put(NAME, name);
+        args.put(PRICE, price);
+        SMLog.i("[UPDATE] "+ id + "  |  " + name + "  |  " + price);
+        return db.update(TABLE_NAME, args, _ID + "=" + id, null) > 0;
+   }
+}
