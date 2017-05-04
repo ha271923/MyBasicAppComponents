d9d76bd9fd745ac63da4e369a81851e24e791f06
hawk_wei <hawk_wei@htc.com>
2017-05-04 19:46:51 +0800

    View: show the Gif animation view on an Activity

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyGifViewActivity.java
@@ -0,0 +1,19 @@
+package sample.hawk.com.mybasicappcomponents.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import sample.hawk.com.mybasicappcomponents.R;
+
+/**
+ * Created by ha271 on 2017/5/4.
+ */
+
+public class MyGifViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mygifviewactivity);
+    }
+}
