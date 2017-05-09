66cde7a71b096f936e56a085a70fb1660214e75a
hawk_wei <hawk_wei@htc.com>
2017-05-09 16:08:52 +0800

    View: include multiple Layout files into a layout

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyLayoutActivity2.java
@@ -0,0 +1,21 @@
+package sample.hawk.com.mybasicappcomponents.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import sample.hawk.com.mybasicappcomponents.R;
+
+/**
+ * Created by ha271 on 2017/5/9.
+ */
+
+public class MyLayoutActivity2 extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mylayout_activity2);
+
+    }
+}
