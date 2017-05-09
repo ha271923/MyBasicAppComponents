a602547937057c49749b5429a01d5570d2e49c61
hawk_wei <hawk_wei@htc.com>
2017-05-09 11:55:12 +0800

    View: RecycleView + CardView sample

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyRecycleCardViewActivity.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyRecycleCardViewActivity.java
@@ -1,8 +1,86 @@
 package sample.hawk.com.mybasicappcomponents.view;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sample.hawk.com.mybasicappcomponents.R;
+
 /**
  * Created by ha271 on 2017/5/9.
  */
 
-public class MyRecycleCardViewActivity {
+public class MyRecycleCardViewActivity extends AppCompatActivity {
+    RecycleCardViewAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myrecyclecardview_activity);
+        ArrayList<String> myDataset = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            myDataset.add(i + "");
+        }
+        mAdapter = new RecycleCardViewAdapter(myDataset);
+        RecyclerView mList = (RecyclerView) findViewById(R.id.list_view);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mList.setLayoutManager(layoutManager);
+        mList.setAdapter(mAdapter);
+    }
+    public class RecycleCardViewAdapter extends RecyclerView.Adapter<RecycleCardViewAdapter.ViewHolder> {
+        private List<String> mData;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView mTextView;
+            public ViewHolder(View v) {
+                super(v);
+                mTextView = (TextView) v.findViewById(R.id.info_text);
+            }
+        }
+
+        public RecycleCardViewAdapter(List<String> data) {
+            mData = data;
+        }
+
+        @Override
+        public RecycleCardViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.myrecyclecardview_item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            holder.mTextView.setText(mData.get(position));
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MyRecycleCardViewActivity.this, "Item " + position + " is clicked.", Toast.LENGTH_SHORT).show();
+                }
+            });
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(MyRecycleCardViewActivity.this, "Item " + position + " is long clicked.", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mData.size();
+        }
+    }
 }
