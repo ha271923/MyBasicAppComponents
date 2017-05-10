196c37cbf6e41cf1e63717a318a7edb1408f31e5
hawk_wei <hawk_wei@htc.com>
2017-05-10 14:51:42 +0800

    View: MyCustomView sample, show a Pie and controllable.

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/view/MyCustomViewActivity.java
@@ -0,0 +1,41 @@
+package sample.hawk.com.mybasicappcomponents.view;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import sample.hawk.com.mybasicappcomponents.R;
+
+/**
+ * Created by ha271 on 2017/5/10.
+ */
+
+public class MyCustomViewActivity extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Resources res = getResources();
+
+        setContentView(R.layout.mycustomviewactivity);
+        final MyCustomView pie = (MyCustomView) this.findViewById(R.id.Pie);
+        pie.addItem("Agamemnon", 2, res.getColor(R.color.seafoam));
+        pie.addItem("Bocephus", 3.5f, res.getColor(R.color.chartreuse));
+        pie.addItem("Calliope", 2.5f, res.getColor(R.color.emerald));
+        pie.addItem("Daedalus", 3, res.getColor(R.color.bluegrass));
+        pie.addItem("Euripides", 1, res.getColor(R.color.turquoise));
+        pie.addItem("Ganymede", 3, res.getColor(R.color.slate));
+
+        ((Button) findViewById(R.id.Reset)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                pie.setCurrentItem(0);
+            }
+        });
+    }
+}
