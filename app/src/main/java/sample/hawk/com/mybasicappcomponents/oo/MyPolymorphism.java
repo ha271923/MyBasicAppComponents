3d35604974cced62a851904504075b3c0235bee4
hawk_wei <hawk_wei@htc.com>
2017-05-17 16:58:21 +0800

    fix

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/oo/MyPolymorphism.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/oo/MyPolymorphism.java
@@ -4,11 +4,8 @@ import android.graphics.Color;
 
 import sample.hawk.com.mybasicappcomponents.Polymorphism.Brian;
 import sample.hawk.com.mybasicappcomponents.Polymorphism.Head;
-import sample.hawk.com.mybasicappcomponents.Polymorphism.Human;
 import sample.hawk.com.mybasicappcomponents.Polymorphism.IHeadActions;
 import sample.hawk.com.mybasicappcomponents.Polymorphism.IIQActions;
-import sample.hawk.com.mybasicappcomponents.oo.ChildClass;
-import sample.hawk.com.mybasicappcomponents.oo.ParentClass;
 
 /**
  * Created by ha271 on 2016/10/17.
@@ -31,25 +28,41 @@ public class MyPolymorphism {
 
     }
 
-    // Runtime Polymorhism( or Dynamic polymorphism )
+    // Runtime Polymorhism( or Dynamic polymorphism ) Trace code by 'CTRL+left-CLICK' may NOT right.
     public void DynamicPolymorphism(){
         Brian obj1 = new Brian(); // Brian ref -> Brian obj
         Brian obj2 = new Head();  // Brian ref -> Head obj
         obj1.think();
-        obj2.think();  // call Head.think()  ***
+        obj2.think();  // call Head.think()  *** If you trace code by 'CTRL+left-CLICK', it will goto Brian.think(), and it's ERROR.
 
+    // A. parent ref point to parent obj
         // obj1.see("Girls");         // ERROR: Brian ref can NOT access entire Head obj. ***
         // ((Head)obj1).see("Girls"); // ERROR: Brian ref CAST to Head ref --> exception.
 
+    // B. parent ref point to child obj
         // obj2.see("Girls");     // ERROR: Brian ref can NOT access entire Head obj. ***
         if( obj2 instanceof Head ){
             ((Head)obj2).see("Girls");// PASS : Brian ref 'CAST to' Head ref --> can access entire Head obj. *** Polymorphism!! ***
         }
 
+    // C. child ref point to child obj
         Head obj3 = new Head();
         if( obj3 instanceof IHeadActions ){ // PASS : Head ref can access entire Head interface included child class. *** Polymorphism!! ***
             obj3.speak();
         }
+
+    // D. interface ref point to class obj
+        IHeadActions IheadAction = new Head(); // PASS : IheadAction Interface ref can access entire Head object.
+        IheadAction.speak();
+        IheadAction.see("Girls");
+        IheadAction.eat("SeaFood");
+        if( IheadAction instanceof IIQActions ) { // PASS : IheadAction Interface ref can use IIQActions ability.
+            ((IIQActions) IheadAction).think();
+        }
+
+    // E. interface ref point to interface obj
+        // IHeadActions IheadAction = new IHeadActions(); // ERROR: IHeadActions is abstract, cannot be instantiated.
+        // IIQActions IiqAction = new IIQActions(); // ERROR: IIQActions is abstract, cannot be instantiated.
 
     }
 
