8fb772a5b84d0f2556d2c55386ba67a6933f76bb
hawk_wei <hawk_wei@htc.com>
2017-06-26 14:37:41 +0800

    Refactor - move all ExoPlayer into Video Folder

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/view/Video/ExoPlayerEventLogger.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/view/Video/ExoPlayerEventLogger.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sample.hawk.com.mybasicappcomponents.view.ExoPlayer;
+package sample.hawk.com.mybasicappcomponents.view.Video;
 
 import android.os.SystemClock;
 import android.util.Log;
