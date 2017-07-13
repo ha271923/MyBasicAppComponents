37dc677be82ad63afb649cfbe5cd4c9ea2813a62
Hawk Wei <ha271923@gmail.com>
2017-07-13 18:14:30 +0800

    IntentService: pass param to stop IntentService

--- app/src/main/java/sample/hawk/com/mybasicappcomponents/background/MyIntentService.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/background/MyIntentService.java
@@ -2,6 +2,10 @@ package sample.hawk.com.mybasicappcomponents.background;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,7 +19,7 @@ import sample.hawk.com.mybasicappcomponents.utils.SMLog;
 public class MyIntentService extends IntentService { //
     private static final String TAG = "MyIntentService";
     private static final SimpleDateFormat SDF_DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss.SSS");
-
+    private static boolean bForceStop=false;
     public MyIntentService() { // in UI thread
         super(TAG);
         SMLog.i(TAG," ----> constructor TID="+ Thread.currentThread().getId());
@@ -30,13 +34,22 @@ public class MyIntentService extends IntentService { //
     @Override
     public void onDestroy() { // in UI thread
         super.onDestroy();
+        bForceStop = false;
         SMLog.i(TAG," ----> onDestroy() TID=" + Thread.currentThread().getId());
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {  // in UI thread
+        // TODO: process and receive the command by setAction() or param by putExtra() at here!!
         SMLog.i(TAG," ----> onStartCommand() TID=" + Thread.currentThread().getId());
-        intent.putExtra("time", System.currentTimeMillis());
+        String action = intent.getAction();
+        SMLog.i(TAG,action);
+        if (action.equals("START_IntentService")) {
+            intent.putExtra("time", System.currentTimeMillis());
+        } else if (action.equals("STOP_IntentService")) {
+            bForceStop = true;
+        }
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -46,19 +59,44 @@ public class MyIntentService extends IntentService { //
         super.setIntentRedelivery(enabled);
     }
 
+    // IntentService automatically stops itself when onHandleIntent() ends, if no more commands had been sent to it
+    // while onHandleIntent() was running. Hence, you do not manually stop an IntentService yourself.
     @Override
     protected void onHandleIntent(Intent intent) { // in Worker thread now!!!
+        // TODO: put your job in workerThread at here!!
         SMLog.i(TAG," ----> onHandleIntent() TID=" + Thread.currentThread().getId());
         // WorkerThread
-        // TODO: put your background job at here!!
-
+        String action = intent.getAction();
+        SMLog.i(TAG,action);
         long time = intent.getLongExtra("time", 0);
         Date date = new Date(time);
         try {
-            SMLog.i(TAG," ----> onHandleIntent(): Date" + SDF_DATE_FORMAT.format(date));
-            Thread.sleep(3000);
+            for(int i=0; i<10; i++){
+                if( bForceStop == true )
+                    break;
+                SMLog.i(TAG,"["+i+"]  ----> onHandleIntent(): Date" + SDF_DATE_FORMAT.format(date));
+                Thread.sleep(1000);
+                updateUiProgressBar(intent , i);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    // send data between Activity and IntentService
+    private void updateUiProgressBar(Intent intent, final int i){
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            Messenger messenger = (Messenger) bundle.get("messenger");
+            Message msg = Message.obtain();
+            bundle.putInt("progress",i);
+            msg.setData(bundle); //put the data here
+            try {
+                messenger.send(msg);
+            } catch (RemoteException e) {
+                SMLog.e("error");
+            }
         }
     }
 }
--- app/src/main/java/sample/hawk/com/mybasicappcomponents/background/MyIntentServiceActivity.java
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/background/MyIntentServiceActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-
 
 import sample.hawk.com.mybasicappcomponents.R;
 import sample.hawk.com.mybasicappcomponents.utils.SMLog;
@@ -21,18 +22,31 @@ import sample.hawk.com.mybasicappcomponents.utils.SMLog;
 
 public class MyIntentServiceActivity extends Activity {
     Context mContext;
-
+    public static ProgressBar myintentservice_progressBar;
     public ToggleButton mMyIntentServiceToggleBtn;
     public TextView mMyOutputTextView;
+    Handler mMainUiHandler;
+
+    interface passDataToIntentService {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = this;
-        setContentView(R.layout.myasynctask);
+        setContentView(R.layout.myintentservice_activity);
         mMyOutputTextView        = (TextView) findViewById(R.id.OutputTextView);
         mMyIntentServiceToggleBtn    = (ToggleButton) findViewById(R.id.IntentServiceToggleBtn);
         mMyIntentServiceToggleBtn.setOnClickListener(mMyIntentServiceToggleBtnListener);
+        myintentservice_progressBar = (ProgressBar) findViewById(R.id.myintentservice_progressBar);
+        mMainUiHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Bundle reply = msg.getData();
+                myintentservice_progressBar.setProgress(reply.getInt("progress"));
+            }
+        };
     }
 
 
@@ -41,14 +55,17 @@ public class MyIntentServiceActivity extends Activity {
         public void onClick(View v) {
 
             Intent intent = new Intent(mContext, MyIntentService.class);
+            intent.putExtra("messenger", new Messenger(mMainUiHandler));
             SMLog.i();
             if(mMyIntentServiceToggleBtn.isChecked()){
+                intent.setAction("START_IntentService");
                 startService(intent);
                 Toast.makeText(mContext, "new IntentService", Toast.LENGTH_SHORT).show();
                 mMyOutputTextView.setText("START IntentService");
             }
             else{
-                // stopService(intent); // Hawk: IntentService can't be stop, href="http://stackoverflow.com/questions/8709989/how-to-stop-intentservice-in-android"
+                intent.setAction("STOP_IntentService");
+                startService(intent); // Hawk: call startService to pass intent with parameter to the running IntentService again.
                 Toast.makeText(mContext, "Can't STOP IntentService", Toast.LENGTH_SHORT).show();
                 mMyOutputTextView.setText("Can't STOP IntentService");
             }
