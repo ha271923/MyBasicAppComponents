a0d4b70c0a79e07687550eb10a8317cdcae199fd
Hawk Wei <hawk_wei@htc.com>
2017-09-13 18:34:00 +0800

    graphic: ScaleType parameter test

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/graphic/MyScaleTypeActivity.java
@@ -0,0 +1,18 @@
+package sample.hawk.com.mybasicappcomponents.graphic;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import sample.hawk.com.mybasicappcomponents.R;
+
+/**
+ * Created by ha271 on 2017/9/12.
+ */
+
+public class MyScaleTypeActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscaletypeactivity);
+    }
+}
