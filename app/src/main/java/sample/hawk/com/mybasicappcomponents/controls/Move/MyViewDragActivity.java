9301a1c3f9bbbcb37f828779881bcd76162db075
Hawk Wei <hawk_wei@htc.com>
2017-09-27 10:54:12 +0800

    Control: use ViewDragHelper to move View

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/controls/Move/MyViewDragActivity.java
@@ -0,0 +1,26 @@
+package sample.hawk.com.mybasicappcomponents.controls.Move;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import sample.hawk.com.mybasicappcomponents.R;
+
+/**
+ * Created by ha271 on 2017/9/26.
+ */
+
+public class MyViewDragActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myviewdragactivity);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/controls/Move/MyViewDragViewGroup.java
@@ -0,0 +1,144 @@
+package sample.hawk.com.mybasicappcomponents.controls.Move;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ha271 on 2017/9/26.
+ */
+
+public class MyViewDragViewGroup extends LinearLayout {
+    private ViewDragHelper mDragger;
+
+    private View mDragView;
+    private View mAutoBackView;
+    private View mEdgeTrackerView;
+
+    private Point mAutoBackOriginPos = new Point();
+
+    public MyViewDragViewGroup(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        mDragger = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback()
+        {
+            @Override
+            public boolean tryCaptureView(View child, int pointerId)
+            {
+                //mEdgeTrackerView禁止直接移动
+                return child == mDragView || child == mAutoBackView;
+            }
+
+            @Override
+            public int clampViewPositionHorizontal(View child, int left, int dx)
+            {
+                return left;
+            }
+
+            @Override
+            public int clampViewPositionVertical(View child, int top, int dy)
+            {
+                return top;
+            }
+
+            @Override
+            public void onViewReleased(View releasedChild, float xvel, float yvel)
+            {
+                //mAutoBackView手指释放时可以自动回去
+                if (releasedChild == mAutoBackView)
+                {
+                    mDragger.settleCapturedViewAt(mAutoBackOriginPos.x, mAutoBackOriginPos.y);
+                    invalidate();
+                }
+            }
+
+            //在边界拖动时回调
+            @Override
+            public void onEdgeDragStarted(int edgeFlags, int pointerId)
+            {
+                mDragger.captureChildView(mEdgeTrackerView, pointerId);
+            }
+/*
+            @Override
+            public void onViewDragStateChanged(int state) {
+                super.onViewDragStateChanged(state);
+            }
+
+            @Override
+            public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+                super.onViewPositionChanged(changedView, left, top, dx, dy);
+            }
+
+            @Override
+            public void onViewCaptured(View capturedChild, int activePointerId) {
+                super.onViewCaptured(capturedChild, activePointerId);
+            }
+
+            @Override
+            public void onEdgeTouched(int edgeFlags, int pointerId) {
+                super.onEdgeTouched(edgeFlags, pointerId);
+            }
+
+            @Override
+            public boolean onEdgeLock(int edgeFlags) {
+                return super.onEdgeLock(edgeFlags);
+            }
+
+            @Override
+            public int getOrderedChildIndex(int index) {
+                return super.getOrderedChildIndex(index);
+            }
+*/
+        });
+        mDragger.setEdgeTrackingEnabled(ViewDragHelper.EDGE_LEFT);
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event)
+    {
+        return mDragger.shouldInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        mDragger.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void computeScroll()
+    {
+        if(mDragger.continueSettling(true))
+        {
+            invalidate();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
+        super.onLayout(changed, l, t, r, b);
+
+        mAutoBackOriginPos.x = mAutoBackView.getLeft();
+        mAutoBackOriginPos.y = mAutoBackView.getTop();
+    }
+
+    @Override
+    protected void onFinishInflate()
+    {
+        super.onFinishInflate();
+
+        mDragView = getChildAt(0);
+        mAutoBackView = getChildAt(1);
+        mEdgeTrackerView = getChildAt(2);
+    }
+
+
+
+}
