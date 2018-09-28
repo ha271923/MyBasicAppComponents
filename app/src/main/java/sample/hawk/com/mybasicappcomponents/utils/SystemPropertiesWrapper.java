a7001c51dc137c696fa8f32a0679c782867ec16a
hawk_wei <hawk_wei@htc.com>
2018-09-28 17:58:45 +0800

    App properties

---
+++ app/src/main/java/sample/hawk/com/mybasicappcomponents/utils/SystemPropertiesWrapper.java
@@ -0,0 +1,21 @@
+package sample.hawk.com.mybasicappcomponents.utils;
+
+// import android.os.SystemProperties;
+
+
+// Can only use it if APP signed platform key.
+public class SystemPropertiesWrapper {
+    /*
+    public static String get(String id, String defaultValue) {
+        return SystemProperties.get(id, defaultValue);
+    }
+
+    public static int getInt(String id, int defaultValue) {
+        return SystemProperties.getInt(id, defaultValue);
+    }
+
+    public static boolean getBoolean(String id, boolean defaultValue) {
+        return SystemProperties.getBoolean(id, defaultValue);
+    }
+    */
+}
